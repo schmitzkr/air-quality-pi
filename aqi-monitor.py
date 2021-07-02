5dabb8de2aa55ff5114773f6144c5a8242ecfb7a
kyle <admin@mael.is>
2021-07-02 14:58:53 -0700

    copied functions from project

--- aqi-monitor.py
+++ aqi-monitor.py
@@ -2,3 +2,39 @@ from sds011 import *
 sensor = SDS011("/dev/ttyUSB0")
 
 pmt_2_5, pmt_10 = sensor.query()
+
+def get_data(n=3):
+    sensor.sleep(sleep=False)
+    pmt_2_5 = 0
+    pmt_10 = 0
+    time.sleep(10)
+    for i in range (n):
+        x = sensor.query()
+        pmt_2_5 = pmt_2_5 + x[0]
+        pmt_10 = pmt_10 + x[1]
+        time.sleep(2)
+    pmt_2_5 = round(pmt_2_5/n, 1)
+    pmt_10 = round(pmt_10/n, 1)
+    sensor.sleep(sleep=True)
+    time.sleep(2)
+    return pmt_2_5, pmt_10
+
+def conv_aqi(pmt_2_5, pmt_10):
+    aqi_2_5 = aqi.to_iaqi(aqi.POLLUTANT_PM25, str(pmt_2_5))
+    aqi_10 = aqi.to_iaqi(aqi.POLLUTANT_PM10, str(pmt_10))
+    return aqi_2_5, aqi_10
+
+def save_log(): 
+    with open("home/grigory/air_quality.csv", "a") as log:
+        dt = datetime.now()
+    log.write("{},{},{},{},{}\n".format(dt, pmt_2_5, aqi_2_5,       pmt_10,aqi_10))
+    log.close()
+
+while(True): 
+    pmt_2_5, pmt_10 = get_data()
+    aqi_2_5, aqi_10 = conv_aqi(pmt_2_5, pmt_10)
+    try:
+        save_log()
+    except:
+        print ("[INFO] Failure in logging data") 
+    time.sleep(60)
