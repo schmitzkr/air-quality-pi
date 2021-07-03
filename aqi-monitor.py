49613821fec40fae518e5bfa0b283b1a7941e1d4
kyle <admin@mael.is>
2021-07-03 07:12:13 -0700

    added correct sds011 package

--- aqi-monitor.py
+++ aqi-monitor.py
@@ -1,23 +1,25 @@
+import time
+from datetime import datetime
 from sds011 import *
-sensor = SDS011("/dev/ttyUSB0")
+import aqi
 
-pmt_2_5, pmt_10 = sensor.query()
+sensor = SDS011("/dev/ttyUSB1")
 
 def get_data(n=3):
-    sensor.sleep(sleep=False)
-    pmt_2_5 = 0
-    pmt_10 = 0
-    time.sleep(10)
-    for i in range (n):
-        x = sensor.query()
-        pmt_2_5 = pmt_2_5 + x[0]
-        pmt_10 = pmt_10 + x[1]
+        sensor.sleep(sleep=False)
+        pmt_2_5 = 0
+        pmt_10 = 0
+        time.sleep(10)
+        for i in range (n):
+            x = sensor.query()
+            pmt_2_5 = pmt_2_5 + x[0]
+            pmt_10 = pmt_10 + x[1]
+            time.sleep(2)
+        pmt_2_5 = round(pmt_2_5/n, 1)
+        pmt_10 = round(pmt_10/n, 1)
+        sensor.sleep(sleep=True)
         time.sleep(2)
-    pmt_2_5 = round(pmt_2_5/n, 1)
-    pmt_10 = round(pmt_10/n, 1)
-    sensor.sleep(sleep=True)
-    time.sleep(2)
-    return pmt_2_5, pmt_10
+        return pmt_2_5, pmt_10
 
 def conv_aqi(pmt_2_5, pmt_10):
     aqi_2_5 = aqi.to_iaqi(aqi.POLLUTANT_PM25, str(pmt_2_5))
@@ -25,9 +27,9 @@ def conv_aqi(pmt_2_5, pmt_10):
     return aqi_2_5, aqi_10
 
 def save_log(): 
-    with open("home/grigory/air_quality.csv", "a") as log:
+    with open("/home/grigory/air_quality.csv", "a") as log:
         dt = datetime.now()
-    log.write("{},{},{},{},{}\n".format(dt, pmt_2_5, aqi_2_5,       pmt_10,aqi_10))
+    log.write("{},{},{},{},{}\n".format(dt, pmt_2_5, aqi_2_5, pmt_10,aqi_10))
     log.close()
 
 while(True): 
